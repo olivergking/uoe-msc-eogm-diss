9ee5cac7031b19d09e45cd92b4c195bd24507a88
oliver01king <oliver01king@gmail.com>
2024-06-02 11:09:52 +0100

    Download script for BedMachine-v5.

---
+++ data/misc/bedmachine/bedmachine-v5_download.bat
@@ -0,0 +1,3 @@
+wget --http-user=oliver01king --http-password=EarthDataSearch15! --keep-session-cookies --no-check-certificate --auth-no-challenge -r --reject "index.html*" -np -e robots=off --no-directories https://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IDBMG4.005/1993.01.01/BedMachineGreenland-v5.nc
+
+
