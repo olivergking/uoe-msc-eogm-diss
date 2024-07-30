a32bcfb82f0f2a632b8b8be3cb674b96bb816205
oliver01king <oliver01king@gmail.com>
2024-07-30 10:34:11 +0100

    Moved unused elevation datasets to scratch.

--- data/elevation/aerodem/aerodem_download.bat
+++
@@ -1,4 +0,0 @@
-wget https://www.ncei.noaa.gov/data/oceans/archive/arc0088/0145405/1.1/data/0-data/G150AERODEM/DEM/aerodem_1981_utm25.tif
-wget https://www.ncei.noaa.gov/data/oceans/archive/arc0088/0145405/1.1/data/0-data/G150AERODEM/DEM/aerodem_1987_utm26_3.tif
-
-
--- data/elevation/grimpdem/grimpdem_download.bat
+++
@@ -1,6 +0,0 @@
-wget --http-user=oliver01king --http-password=EarthDataSearch15! --keep-session-cookies --no-check-certificate --auth-no-challenge -r --reject "index.html*" -np -e robots=off --no-directories https://n5eil01u.ecs.nsidc.org/MEASURES/NSIDC-0645.001/2003.02.20/gimpdem4_2_v01.1.tif
-wget --http-user=oliver01king --http-password=EarthDataSearch15! --keep-session-cookies --no-check-certificate --auth-no-challenge -r --reject "index.html*" -np -e robots=off --no-directories https://n5eil01u.ecs.nsidc.org/MEASURES/NSIDC-0645.001/2003.02.20/gimpdem4_3_v01.1.tif
-wget --http-user=oliver01king --http-password=EarthDataSearch15! --keep-session-cookies --no-check-certificate --auth-no-challenge -r --reject "index.html*" -np -e robots=off --no-directories https://n5eil01u.ecs.nsidc.org/MEASURES/NSIDC-0715.002/2008.05.15/tile_4_2_30m_dem_v02.0.tif
-wget --http-user=oliver01king --http-password=EarthDataSearch15! --keep-session-cookies --no-check-certificate --auth-no-challenge -r --reject "index.html*" -np -e robots=off --no-directories https://n5eil01u.ecs.nsidc.org/MEASURES/NSIDC-0715.002/2008.05.15/tile_4_3_30m_dem_v02.0.tif
-
-
